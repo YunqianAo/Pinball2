dfa8a1b4c7699a3111e2ae55a1bc9479a43949ec
YunqianAo <79161526+YunqianAo@users.noreply.github.com>
2021-10-28 12:01:33 +0200

    Create new funtion of Chain

--- Physics II - Box2D - Handout 04/ModulePhysics.h
+++ Physics II - Box2D - Handout 04/ModulePhysics.h
@@ -46,6 +46,7 @@ public:
 	PhysBody* CreateRectangle(int x, int y, int width, int height);
 	PhysBody* CreateRectangleSensor(int x, int y, int width, int height);
 	PhysBody* CreateChain(int x, int y, int* points, int size);
+	PhysBody* CreateChain2(int x, int y, int* points, int size);
 
 	// b2ContactListener ---
 	void BeginContact(b2Contact* contact);
